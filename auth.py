bd8e87b149661b770f5b6802aeccf9582591a9f9
pranavisamhita <159760086+pranavisamhita@users.noreply.github.com>
2026-01-29 13:36:19 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,28 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from database import SessionLocal
+import models
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+fake_users_db = {
+    "admin": {
+        "username": "admin",
+        "password": "admin123"
+    }
+}
+
+def authenticate_user(username: str, password: str):
+    user = fake_users_db.get(username)
+    if not user or user["password"] != password:
+        return False
+    return user
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    if token != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication"
+        )
+    return token
